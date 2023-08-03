9eecb93359762308e4fbf1f22177a0633134a563
astro175 <aspark400@gmail.com>
2023-08-03 17:55:11 +0100

    added task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Typecasts a float to a string"""
+
+def to_str(n: float) -> str:
+    """
+       A type-annotated function to_str that takes a float n
+       as argument and returns the string representation 
+       of the float.
+    """
+    return str(n)
