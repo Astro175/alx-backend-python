a4d85a352c72fdd73b8e82bba8d2f21d7763fd80
astro175 <aspark400@gmail.com>
2023-08-03 23:40:59 +0100

    fixed task 5, using python 3.8 method for list annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """Returns sum of variable in a list"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
       A type-annotated function sum_list which takes
       a list input_list of floats as argument
