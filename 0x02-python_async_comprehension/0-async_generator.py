2816aa4ff3142de68b10058776404c03267be9c1
astro175 <aspark400@gmail.com>
2023-08-08 15:57:04 +0100

    added task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Module that creates a coroutine"""
+import random
+import asyncio
+
+
+async def async_generator() -> float: 
+    """
+       The coroutine will loop 10 times, each time asynchronously wait 1
+       second, then yield a random number between 0 and 10.
+       Use the random module.
+    """
+    for i in range(11):
+        await asyncio.sleep(1)
+        num = random.uniform(0, 10)
+        yield num
