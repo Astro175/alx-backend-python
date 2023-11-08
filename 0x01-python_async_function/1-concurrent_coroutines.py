837f6c8dd332286c1004f6cdc8bfd95d94dfce47
astro175 <aspark400@gmail.com>
2023-11-08 10:27:48 +0100

    fixed pep8

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-import asyncio
+"""Import wait_random from the previous python file that you’ve
+written and write an async routine called wait_n that takes in 2
+int arguments (in this order): n and max_delay. You will spawn
+wait_random n times with the specified max_delay.
+
+wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order without using
+sort() because of concurrency.
 """
-   Execute multiple coroutines at the same time with async.
-"""
+
 from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
@@ -26,6 +34,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         num_list.append(coro)
 
     return num_list
-
-
-
